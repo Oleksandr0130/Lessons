abbabc2429abc939c367f41c3c47b9bf803cc042
Oleksandr <olek1995sta@gmail.com>
2024-02-05 19:58:14 +0100

    homework

--- src/lessons_14/HomeWork/task1.java
+++ src/lessons_14/HomeWork/task1.java
@@ -1,2 +1,46 @@
-package lessons_14.HomeWork;public class task1 {
-}
+package lessons_14.HomeWork;
+
+
+
+//Написать метод swap, принимающий на вход массив целых чисел, и два индекса.
+// Метод должен поменять местами значения, хранящихся в этих индексах
+//
+//{0, 1, 2, 3, 4, 5, 6} -> swap(arr, 1, 5} -> {0, 5, 2, 3, 4, 1, 6}
+//Если индексы за пределами массива?
+//
+//Перегрузка метода для работы с массивами строк.
+public class task1 {
+    public static void main(String[] args) {
+        int[] ints = {0, 1, 2, 3, 4, 5, 6};
+        printArray(ints);
+        swap(ints,1, 5, 3, 4);
+        printArray(ints);
+
+
+    }
+
+
+    public static void printArray(int[] arr) {
+
+        System.out.print("[");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + ((i < arr.length - 1) ? ", " : "]\n"));
+        }
+    }
+
+    public static void swap(int[] arr, int index1, int index2, int index3, int index4) {
+        if (index1 >= 0 && index1 < arr.length &&
+                index2 >= 0 && index2 < arr.length &&
+                index3 >= 0 && index3 < arr.length &&
+                index4 >= 0 && index4 < arr.length) {
+            int temp = arr[index1];
+            int temp1 = arr[index3];
+            arr[index1] = arr[index2];
+            arr[index3] = arr[index4];
+            arr[index2] = temp;
+            arr[index4] = temp1;
+            }else {
+            System.out.println("Ошибка! Замена не произведена.");
+        }
+        }
+    }
---
+++ src/lessons_14/HomeWork/task2.java
@@ -0,0 +1,23 @@
+package lessons_14.HomeWork;
+
+public class task2 {
+    public static void main(String[] args) {
+        String str = "0123456789";
+        System.out.println(substring(str, 2, 5));
+
+    }
+
+    public static String substring(String string, int startIdx, int endIdx){
+        String result = "";
+        if (string == null || startIdx < 0 || endIdx >= string.length() || endIdx <= startIdx ) return result;
+
+        char[] chars = new char[endIdx - startIdx];
+
+        for (int i = 0; i < chars.length; i++){
+            chars[i] = string.charAt(i + startIdx);
+        }
+       // return result;
+        return String.valueOf(chars);
+    }
+
+}
