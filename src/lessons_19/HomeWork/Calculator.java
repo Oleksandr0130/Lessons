182100e9646b5557ad949889f263da7418a2c16f
Oleksandr <olek1995sta@gmail.com>
2024-02-12 17:53:40 +0100

    new

--- src/lessons_19/HomeWork/Calculator.java
+++ src/lessons_19/HomeWork/Calculator.java
@@ -13,7 +13,7 @@ public class Calculator {
     }
 
     public static double subtraction(double num1, double num2) {
-        return num1 + num2;
+        return num1 - num2;
     }
 
     public static double multiplication(double num1, double num2) {
