e33dabeb856a8f201db20e6fded8718aea45f3b5
Oleksandr0130 <olek1995sta@gmail.com>
2024-02-20 17:27:46 +0100

    new

---
+++ src/lessons_25/HomeWork/MainRubber.java
@@ -0,0 +1,12 @@
+package lessons_25.HomeWork;
+
+public class MainRubber {
+    public static void main(String[] args) {
+        RubberArray array = new RubberArray<>();
+        array.add(5);
+        array.add(5);
+        array.add(5);
+        array.add(5);
+        System.out.println(array.toString());
+    }
+}
